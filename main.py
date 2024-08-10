4b36d7c33612cd047c7eed97606265b9dd97243f
AmanoSpica <hebimeme@gmail.com>
2024-08-10 12:42:16 +0900

    uploaderを分ける処理を追加

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ from modules.youtube_uploader import YoutubeVideoManager
 
 
 load_dotenv(find_dotenv())
-print("API Key: " + os.getenv("YOUTUBE_API_KEY"))
 
 get_video_data = GetVideoData(
     api_key=os.getenv("YOUTUBE_API_KEY"),
@@ -32,9 +31,16 @@ db = DBManager(
     db_database=os.getenv("DB_DATABASE")
 )
 
+identity_files = []
+for i in range(1, 11):
+    file_name = f"identity/client_secret_YoutubeArchiver-uploader{str(i).zfill(2)}.json"
+    if os.path.exists(file_name):
+        identity_files.append(file_name)
+
 youtube = YoutubeVideoManager(
     upload_channel_id=os.getenv("UPLOAD_YOUTUBE_CHANNEL_ID"),
-    identity_file="client_secret.json"
+    identity_file="identity/client_secret.json",
+    uploader_identity_file=identity_files
 )
 
 
@@ -119,6 +125,32 @@ def into_str(video):
     return columns
 
 
+def format_video_info(video_data: dict):
+    title = video_data["title"]
+    video_info = ""
+    if video_data["videoType"] == "shorts":
+        video_info += "【#Shorts】\n"
+        title += "  #Shorts"
+    elif video_data["videoType"] == "liveArchive":
+        video_info += "【配信アーカイブ または プレミア公開 動画】\n"
+        video_info += f"配信・公開予定日時: {format_datetime(video_data['liveStreamingDetails_scheduledStartTime'])}\n" if video_data["liveStreamingDetails_scheduledStartTime"] is not None else "配信・公開予定日時: [指定なし]\n"
+        video_info += f"配信・公開開始日時: {format_datetime(video_data['liveStreamingDetails_actualStartTime'])}\n"
+        video_info += f"配信・公開終了日時: {format_datetime(video_data['liveStreamingDetails_actualEndTime'])}\n"
+    elif video_data["videoType"] == "video":
+        video_info += "【通常動画】\n"
+    else:
+        raise "Error: videoType is invalid."
+
+    video_info += f"投稿日時: {format_datetime(video_data['publishedAt'])}\n"
+    video_info += f"再生回数: {insert_comma(video_data['viewCount'])} 回\n" if video_data["viewCount"] is not None else "再生回数: [非公開]\n"
+    video_info += f"高評価数: {insert_comma(video_data['likeCount'])} 件\n" if video_data["likeCount"] is not None else "高評価数: [非公開]\n"
+    video_info += f"コメント数: {insert_comma(video_data['commentCount'])} 件\n" if video_data["commentCount"] is not None else "コメント数: [コメント無効]\n"
+    video_info += f"※ データは取得時点({datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')})のものです。\n\nこの動画は「Aqua Ch. 湊あくあ」さんの公式チャンネルから取得したアーカイブ動画です。"
+    video_info = f"{'#'*20}\n\n{video_info}\n\n{'#'*20}\n\n\n\n{video_data['description']}"
+    return title, video_info
+
+
+
 async def save_to_database(json_data):
     chunks_data = list(chunks(json_data, 30))
     for chunk in chunks_data:
@@ -155,19 +187,113 @@ async def save_to_database(json_data):
         await db.query(query)
 
 
-def download_and_upload():
+def download_video(video: pd.DataFrame, startTime: float, number: int, totalVideos: int):
+    cprint(f"\nProgress:  ({number + 1}/{totalVideos}) {video['title']}", attrs=[Color.BRIGHT_YELLOW])
+
+    progress_time = time.time()
+    video_file_path = download_youtube_video(video["id"], "temp/videos")
+    cprint(f"Downloaded: {video_file_path}", attrs=[Color.BRIGHT_GREEN])
+    print(f"Time: {time.time() - progress_time:.2f} sec")
+    print(f"Total Time: {time.time() - startTime:.2f} sec\n")
+
+    progress_time = time.time()
+    thumbnail_file_path = download_youtube_thumbnail(video["id"], "temp/thumbnails", video["thumbnails_url"])
+    cprint(f"Downloaded: {thumbnail_file_path} (thumbnail)", attrs=[Color.BRIGHT_GREEN])
+    print(f"Time: {time.time() - progress_time:.2f} sec")
+    print(f"Total Time: {time.time() - startTime:.2f} sec\n")
+
+    # Update database
+    asyncio.run(
+        db.query(
+            f"UPDATE TargetVideo SET isDownloaded = 1 WHERE id = '{video['id']}';"
+        )
+    )
+    return video_file_path, thumbnail_file_path
+
+
+def upload_video(video: pd.DataFrame, video_file_path: str, thumbnail_file_path: str, startTime: float):
+    progress_time = time.time()
+    cprint(f"Upload Progress: {video['title']}", attrs=[Color.BRIGHT_YELLOW])
+
+    title, description = format_video_info(video)
+    youtube.upload_video(
+        video_file_path=video_file_path,
+        title=title,
+        description=description,
+        category_id=video["categoryId"],
+        thumbnail_file_path=thumbnail_file_path,
+        tags=video["tags"]
+    )
+
+    cprint(f"Uploaded: {video['title']}", attrs=[Color.BRIGHT_GREEN])
+    print(f"Time: {time.time() - progress_time:.2f} sec")
+    print(f"Total Time: {time.time() - startTime:.2f} sec\n")
+
+    # Update database
+    asyncio.run(
+        db.query(
+            f"UPDATE TargetVideo SET isPushed = 1 WHERE id = '{video['id']}';"
+        )
+    )
+
+
+def delete_temp_files(video: pd.DataFrame, video_file_path: str, thumbnail_file_path: str):
+    temp_files = [video_file_path, thumbnail_file_path]
+    for temp_file in temp_files:
+        os.remove(temp_file)
+        print(f"Removed: {temp_file}")
+        # Update database
+        asyncio.run(
+            db.query(
+                f"UPDATE TargetVideo SET isDownloaded = 0 WHERE id = '{video['id']}';"
+            )
+        )
+
+
+
+def CLI_dl_and_up():
     cprint("Youtube動画データを取得・更新しました。", attrs=[Color.BRIGHT_GREEN])
     cprint("ダウンロードを開始するにはEnterキーを押してください。", attrs=[Color.BRIGHT_GREEN], end="")
     input()
 
     cprint("\n何本の動画をダウンロードしますか？ (投稿日時が古いものから処理します): ", attrs=[Color.CYAN], end="")
     output_video_number = input()
+    if not output_video_number <= 60:
+        cprint("60本以上の動画をダウンロードすることはできません。最大の60本を処理します。", attrs=[Color.RED])
+        output_video_number = 60
     cprint("ダウンロードした動画をアップロードしますか？ (y/n): ", attrs=[Color.CYAN], end="")
     is_upload = input()
 
     if is_upload == "y" or is_upload == "Y" or is_upload == "yes" or is_upload == "Yes":
         is_upload = True
         cprint("\nダウンロード・アップロードを開始します。", attrs=[Color.MAGENTA])
+
+        remain_videos = asyncio.run(
+            db.query(
+                "SELECT * FROM TargetVideo WHERE isDownloaded = 1 AND isPushed = 0 ORDER BY publishedAt ASC;"
+            )
+        )
+
+        if not remain_videos.empty:
+            cprint(f"前回の処理でアップロードされていない動画が{remain_videos}本あります。", attrs=[Color.BRIGHT_RED])
+            cprint("アップロードを続行しますか？ (y/n): ", attrs=[Color.CYAN], end="")
+            is_upload_remain = input()
+            if is_upload_remain == "y" or is_upload_remain == "Y" or is_upload_remain == "yes" or is_upload_remain == "Yes":
+                startTime = time.time()
+                for i, video in remain_videos.iterrows():
+                    progress_time = time.time()
+                    video_file_path = f"temp/videos/{video['id']}.mp4"
+                    thumbnail_file_path = f"temp/thumbnails/{video['id']}.jpg"
+                    upload_video(video, video_file_path, thumbnail_file_path, startTime)
+                    delete_temp_files(video, video_file_path, thumbnail_file_path)
+
+                    cprint(f"Finished: {video['title']}", attrs=[Color.GREEN])
+                    print(f"Time: {time.time() - progress_time:.2f} sec")
+                    print(f"Total Time [remained_videos]: {time.time() - startTime:.2f} sec\n")
+
+                cprint("\n前回の処理でアップロードされていなかった動画のアップロードが完了しました。", attrs=[Color.MAGENTA])
+                print(f"Total Time [remained_videos]: {time.time() - startTime:.2f} sec\n")
+
     else:
         is_upload = False
         cprint("\nダウンロードを開始します。", attrs=[Color.BRIGHT_YELLOW])
@@ -178,96 +304,18 @@ def download_and_upload():
         )
     )
 
-    print(f"\nGetting {len(target_videos)} videos to download...")
+    print(f"\nGetting {len(target_videos)} videos...")
     startTime = time.time()
     for i, video in target_videos.iterrows():
-        video_time = time.time()
-
-        cprint(f"\nProgress:  ({i + 1}/{len(target_videos)}) {video['title']}", attrs=[Color.BRIGHT_YELLOW])
-
         progress_time = time.time()
-        video_file_path = download_youtube_video(video["id"], "temp/videos")
-        cprint(f"Downloaded: {video_file_path}", attrs=[Color.BRIGHT_GREEN])
-        print(f"Time: {time.time() - progress_time:.2f} sec")
-        print(f"Total Time: {time.time() - startTime:.2f} sec\n")
+        video_file_path, thumbnail_file_path = download_video(video, startTime, i, len(target_videos))
 
-        progress_time = time.time()
-        thumbnail_file_path = download_youtube_thumbnail(video["id"], "temp/thumbnails", video["thumbnails_url"])
-        cprint(f"Downloaded: {thumbnail_file_path} (thumbnail)", attrs=[Color.BRIGHT_GREEN])
-        print(f"Time: {time.time() - progress_time:.2f} sec")
-        print(f"Total Time: {time.time() - startTime:.2f} sec\n")
-
-        # Update database
-        asyncio.run(
-            db.query(
-                f"UPDATE TargetVideo SET isDownloaded = 1 WHERE id = '{video['id']}';"
-            )
-        )
-
-        progress_time = time.time()
         if is_upload:
-            cprint(f"Upload Progress: {video['title']}", attrs=[Color.BRIGHT_YELLOW])
-
-            title = video["title"]
-            video_info = ""
-            if video["videoType"] == "shorts":
-                video_info += "【#Shorts】\n"
-                title += "  #Shorts"
-            elif video["videoType"] == "liveArchive":
-                video_info += "【配信アーカイブ または プレミア公開 動画】\n"
-                video_info += f"配信・公開予定日時: {format_datetime(video['liveStreamingDetails_scheduledStartTime'])}\n" if video["liveStreamingDetails_scheduledStartTime"] is not None else "配信・公開予定日時: [指定なし]\n"
-                video_info += f"配信・公開開始日時: {format_datetime(video['liveStreamingDetails_actualStartTime'])}\n"
-                video_info += f"配信・公開終了日時: {format_datetime(video['liveStreamingDetails_actualEndTime'])}\n"
-            elif video["videoType"] == "video":
-                video_info += "【通常動画】\n"
-            else:
-                input("Error: videoType is invalid. Press Enter to continue.")
-                continue
-
-            video_info += f"投稿日時: {format_datetime(video['publishedAt'])}\n"
-            video_info += f"再生回数: {insert_comma(video['viewCount'])} 回\n" if video["viewCount"] is not None else "再生回数: [非公開]\n"
-            video_info += f"高評価数: {insert_comma(video['likeCount'])} 件\n" if video["likeCount"] is not None else "高評価数: [非公開]\n"
-            video_info += f"コメント数: {insert_comma(video['commentCount'])} 件\n" if video["commentCount"] is not None else "コメント数: [コメント無効]\n"
-            video_info += f"※ データは取得時点({datetime.datetime.now().strftime('%Y/%m/%d %H:%M:%S')})のものです。\n\nこの動画は「Aqua Ch. 湊あくあ」さんの公式チャンネルから取得したアーカイブ動画です。"
-
-            # print(f"Title: {title}")
-            # print(f"{'#'*20}\n\n{video_info}\n\n{'#'*20}\n\n\n\n{video['description']}")
-            # print(f"Tags: {video['tags']}")
-            # print(f"Category: {video['categoryId']}")
-            youtube.upload_video(
-                video_file_path=video_file_path,
-                title=title,
-                description=f"{'#'*20}\n\n{video_info}\n\n{'#'*20}\n\n\n\n{video['description']}",
-                category_id=video["categoryId"],
-                thumbnail_file_path=thumbnail_file_path,
-                tags=video["tags"]
-            )
-
-            cprint(f"Uploaded: {video['title']}", attrs=[Color.BRIGHT_GREEN])
-            print(f"Time: {time.time() - progress_time:.2f} sec")
-            print(f"Total Time: {time.time() - startTime:.2f} sec\n")
-
-            # Update database
-            asyncio.run(
-                db.query(
-                    f"UPDATE TargetVideo SET isPushed = 1 WHERE id = '{video['id']}';"
-                )
-            )
-
-            temp_files = [video_file_path, thumbnail_file_path]
-            for temp_file in temp_files:
-                os.remove(temp_file)
-                print(f"Removed: {temp_file}")
-
-                # Update database
-                asyncio.run(
-                    db.query(
-                        f"UPDATE TargetVideo SET isDownloaded = 0 WHERE id = '{video['id']}';"
-                    )
-                )
+            upload_video(video, video_file_path, thumbnail_file_path, startTime)
+            delete_temp_files(video, video_file_path, thumbnail_file_path)
 
         cprint(f"Finished: {video['title']}", attrs=[Color.GREEN])
-        print(f"Time: {time.time() - video_time:.2f} sec")
+        print(f"Time: {time.time() - progress_time:.2f} sec")
         print(f"Total Time: {time.time() - startTime:.2f} sec\n")
 
 
@@ -280,7 +328,7 @@ def main():
     get_video_data.save_video_data()
     video_data = load_json("data/videos.json")
     asyncio.run(save_to_database(video_data))
-    download_and_upload()
+    CLI_dl_and_up()
 
 
 
--- modules/youtube_uploader.py
+++ modules/youtube_uploader.py
@@ -9,18 +9,30 @@ import googleapiclient.errors
 scopes = ["https://www.googleapis.com/auth/youtube.upload"]
 
 
+
+
+
 class YoutubeVideoManager:
     def __init__(self,
                  upload_channel_id: str,
                  identity_file: str = "client_secret.json",
-                 ):
-        flow = InstalledAppFlow.from_client_secrets_file(
-            identity_file, scopes)
+                 uploader_identity_file: list[str] = None):
+        flow = InstalledAppFlow.from_client_secrets_file(identity_file, scopes)
         credentials = flow.run_local_server()
-        self.youtube = build(
-            "youtube", "v3", credentials=credentials)
+        self.youtube = build("youtube", "v3", credentials=credentials)
+
+        self.uploader = []
+        if not uploader_identity_file:
+            self.uploader.append(self.youtube)
+        else:
+            for identity in uploader_identity_file:
+                flow = InstalledAppFlow.from_client_secrets_file(identity, scopes)
+                credentials = flow.run_local_server()
+                self.uploader.append(build("youtube", "v3", credentials=credentials))
 
         self.upload_channel_id = upload_channel_id
+        self.number_of_upload_video = 0
+        self.number_of_uploader = 0
 
 
     def upload_video(self,
@@ -30,7 +42,17 @@ class YoutubeVideoManager:
                      category_id: str,
                      thumbnail_file_path: str = None,
                      tags: list[str] = []):
-        request = self.youtube.videos().insert(
+
+        self.number_of_upload_video += 1
+        if self.number_of_uploader > len(self.uploader):
+            raise Exception("The number of videos uploaded has exceeded the limit.")
+
+        if self.number_of_upload_video % len(self.uploader) == 0:
+            self.number_of_uploader += 1
+
+        yt_uploader = self.uploader[self.number_of_uploader]
+
+        request = yt_uploader.videos().insert(
             part = "snippet,status",
             body = {
                 "snippet": {
